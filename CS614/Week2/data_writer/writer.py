e17f30f32d6757b36eb1f9760243b9cdb170e0c6
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-10-19 21:38:00 +0200

    Problem 6 solved.

--- CS614/Week2/data_writer/writer.py
+++ CS614/Week2/data_writer/writer.py
@@ -42,3 +42,16 @@ class DataWriter(object):
                 writer.write("\n")
                 writer.write("Memory Used: " + str(memory_used) + " MB")
 
+        if "Problem6" in self.__solution_dir:
+            with open(os.path.join(self.__solution_dir, "output" + str(file_index) + ".txt"), 'w') as writer:
+                for clump in output_:
+                    writer.write(clump)
+                    writer.write("\n")
+
+                writer.write("\n\n\n")
+                writer.write("======")
+                writer.write("\n")
+                writer.write("Execution Time: " + str(running_time) + " s")
+                writer.write("\n")
+                writer.write("Memory Used: " + str(memory_used) + " MB")
+
--- CS614/Week2/problem6.py
+++ CS614/Week2/problem6.py
@@ -28,6 +28,36 @@ def main():
 
     print("Passed training data..")
 
+    writer = DataWriter(solution_dir)
+    usage = Usage()
+
+    for test_i in range(len(test_cases) - len(output), len(test_cases)):
+        usage.start()
+        case = test_cases[test_i]
+        genome = case[0]
+        k = case[1][0]
+        l = case[1][1][0]
+        t = case[1][1][1]
+        dna = DNA(genome)
+        clumps_patterns = dna.get_clumps_patterns(int(k), int(t), int(l))
+        usage.end()
+
+        writer.write_data(test_i, clumps_patterns, usage.get_execution_time(), usage.get_memory_usage())
+        print("\n\nInput:\n" + genome + "\n" + str(k) + " " + str(l) + " " + str(t))
+
+        print("\n\nOutput")
+        print("=====")
+
+        for clump in clumps_patterns:
+            print(clump)
+
+        print("\n")
+        print("======")
+        print("Execution Time: " + str(usage.get_execution_time()) + " s")
+        print("Memory Used: " + str(usage.get_memory_usage()) + " MB")
+
+
+
 
 if __name__ == '__main__':
     main()
