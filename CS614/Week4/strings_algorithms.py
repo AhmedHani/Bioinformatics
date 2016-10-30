cfbea6020b74ec16d6895b96f449d0a111cc4245
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-10-30 23:34:45 +0200

    strings algorithms file added.

---
+++ CS614/Week4/strings_algorithms.py
@@ -0,0 +1,22 @@
+___author__ = 'Ahmed Hani Ibrahim'
+
+
+class StringsAlgorithms(object):
+    def __init__(self, alpha_string="", beta_string=""):
+        self.__alpha_string = alpha_string
+        self.__beta_string = beta_string
+
+    def lcs(self, algorithm='dp'):
+        if algorithm is 'dp':
+            pass
+        elif algorithm is 'suf':
+            pass
+
+        pass
+
+    def alignment(self, algorithm='leve'):
+        if algorithm is 'leve':
+            pass
+        elif algorithm is 'nwun':
+            pass
+        pass
